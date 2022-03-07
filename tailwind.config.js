b878d549eeb8b5ea631df40a604f36b80342abcc
Sridhar Chandrasekar <75136330+Sridhar-C-25@users.noreply.github.com>
2022-03-07 13:16:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
+module.exports = {
+  mode: "jit",
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [Myclass],
+};
